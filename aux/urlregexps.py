52e26eb5c7dcf1bc7dcc0e84dafb5d917817eb0c
Isaac Dupree <antispam@idupree.com>
2014-01-07 20:49:21 -0500

    Fix urlregexps
    The characters
      ` | %
    hadn't been correctly accounted for.  Also, non-'byte'-named
    regexps still were of byte types (although unused in the code);
    fixed to be str types.  Fix clearlydirectoryurl for URLs with
    query and/or fragment parts.

--- aux/urlregexps.py
+++ aux/urlregexps.py
@@ -1,17 +1,28 @@
 
-# Character-based REs are listing out the alphabet rather than a-zA-Z0-9 so that locale doesn't affect them.
-# see RFC 3986
-urlbyte = br'[^\0- "<>\\^{}\177]'
+# See RFC 3986 and http://url.spec.whatwg.org/
+
+# These *_*set variables are suitable for putting inside
+# a regex's [...] or [^...] set (and not suitable for much else).
+nonurl_byteset = br'\0- "<>\\^`|{}\177'
+nonurl_charset = nonurl_byteset.decode()
+urlbyte = br'[^'+nonurl_byteset+br']'
 urlbytes = urlbyte + br'+'
-urlchar = br"""[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-._~:/?#[\]@!$&'()*+,;=]"""
-schemechar = br"""[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-+.]"""
+# this wrongly excludes non-ASCII characters ( see http://url.spec.whatwg.org/#url-code-points );
+# listing out the alphabet rather than a-zA-Z0-9 so that locale doesn't affect it:
+#urlchar = r"""[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-._~:/?#[\]@!$&'()*+,;=%]"""
+urlchar = r'[^'+nonurl_charset+r']'
+nonurlchar = r'['+nonurl_charset+r']'
+urlchar_sans_single_quote = r'[^'+nonurl_charset+r"']"
+urlchar_sans_parentheses = r'[^'+nonurl_charset+r'()]'
+schemechar = r"""[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-+.]"""
 # These are to be applied to strings that already match urlchar* :
-urlwithdomain = br'^//|^' + schemechar + br'+:'
-domainrelativeurl = br'^/$|^/[^/]'
-samepageurl = br'^#|^$'
-clearlydirectoryurl = br'/$|(^|/)\.\.?$'  #uh what if the trailing slash is in a ? or # part? hm.
+urlwithdomain = r'^//|^' + schemechar + r'+:'
+domainrelativeurl = r'^/$|^/[^/]'
+samepageurl = r'^#|^$'
+# clearlydirectoryurl: path ends in any of / . ..
+clearlydirectoryurl = r'^[^?#]*(?:/|(^|/)\.\.?)(?:[?#]|$)'
 
 # likely ext: ends in .alphanumeric with at least one non-numeric character
-extension = br""".\.[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]*[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz][ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]*$"""
+extension = r""".\.[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]*[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz][ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]*$"""
 
 
