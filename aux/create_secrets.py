095ff21f6997510e084ccc8cc7dbd54c92e6652c
Isaac Dupree <antispam@idupree.com>
2014-01-07 20:51:06 -0500

    export alnum and alnum_secret() from create_secrets.py

--- aux/create_secrets.py
+++ aux/create_secrets.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import random
 
-_alnum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-def _alnum_secret(length = 22):
+alnum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+def alnum_secret(length = 22):
   """
   The default length of 22 gives at least 128 bits of entropy
   (entropy = log2(62**length))
   """
   rng = random.SystemRandom()
-  return ''.join(rng.choice(_alnum) for _ in range(length))
+  return ''.join(rng.choice(alnum) for _ in range(length))
 
 def main():
   for k, v in (
@@ -16,14 +16,14 @@ def main():
       # somewhere and someone knows all but one of the other secrets
       # and uses that to brute-force the last one.  At the beginning
       # and the end for extra paranoia.
-      ('unused_random_string_1', _alnum_secret()),
+      ('unused_random_string_1', alnum_secret()),
       # this is not really random bytes, but who cares
-      ('rr_hash_random_bytes', _alnum_secret().encode('ascii')),
-      ('nginx_hash_random_bytes', _alnum_secret().encode('ascii')),
-      ('errdocs_random_string', _alnum_secret()),
-      ('apache_random_query_string_1', _alnum_secret()),
-      ('apache_random_query_string_2', _alnum_secret()),
-      ('unused_random_string_2', _alnum_secret())):
+      ('rr_hash_random_bytes', alnum_secret().encode('ascii')),
+      ('nginx_hash_random_bytes', alnum_secret().encode('ascii')),
+      ('errdocs_random_string', alnum_secret()),
+      ('apache_random_query_string_1', alnum_secret()),
+      ('apache_random_query_string_2', alnum_secret()),
+      ('unused_random_string_2', alnum_secret())):
     print(k+' = '+repr(v))
 
 if __name__ == '__main__':
